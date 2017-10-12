2d3bbfe4e63f1f11a242b5f6792a357c49fe7979
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-12 11:38:12 +0100

    started reworking code for predefined variables and new goal

--- Week3/ex9.py
+++ Week3/ex9.py
@@ -1,21 +1,22 @@
 ## home cost calculator ##
-print ("lets calculate how long it will take to save up for a house deposit including a semi-annual raise")
+print ("lets calculate how what percenatge of income is needed to save for a house")
 
 #input variables#
 annual_salary       = float(input ("Enter your annual salary: "))
-portion_saved       = float(input("Enter the percent of your salary to save, as a decimal: "))
-
-total_cost          = float(input("Enter the total cost of your dream home: "))
-semi_annual_raise   = float(input("Enter semi-annual raise, as a decimal: "))
-
 
 #define variables#
-portion_deposit     = (total_cost / 100) * 20
-monthly_salary      = annual_salary/12
-monthly_contrib     = monthly_salary * portion_saved
-current_savings     = float(0)
-months_passed       = int(0)
-interest_rate       = 0.04 #rate of return
+total_cost          = float(1000000))                                       #set cost of house
+semi_annual_raise   = float(0.07)                                           #set semi annual raise
+interest_rate       = 0.04                                                  #rate of return
+months_passed       = 36                                                    #define months passed
+
+portion_deposit     = (total_cost / 100) * 25                               #set deposit to 25% of house cost
+monthly_salary      = annual_salary/12                                      #set monthly salary
+monthly_contrib     = monthly_salary * portion_saved                        #set ammount to increase savings by each month
+current_savings     = float(0)                                              #define savings as a float
+portion_saved       = float(0)                                              #defne percentage to save as a float
+
+
 
 #computation loop#
 while current_savings < portion_deposit:
