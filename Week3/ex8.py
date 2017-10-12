89e1e71d8e197ebc1550e72aafd30fc6239e4258
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-12 11:25:43 +0100

    3rd attemptm at ex8, solved by redifiningn monthly_contrib

--- Week3/ex8.py
+++ Week3/ex8.py
@@ -24,7 +24,8 @@ while current_savings < portion_deposit:
     months_passed    = months_passed   + 1                                   #increment month counter
 
     if months_passed % 6 == 0 :
-        annual_salary += annual_salary*semi_annual_raise
-        monthly_salary      = annual_salary/12
+        annual_salary  += annual_salary*semi_annual_raise
+        monthly_salary  = annual_salary/12
+        monthly_contrib = monthly_salary * portion_saved
 
 print (f"Number of months: {months_passed}")                                #output number of months
