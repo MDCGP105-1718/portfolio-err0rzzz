c6f95dd7d1efc5f9a0182f567da9885fe70d8d9e
err0rzzz <s190988@uos.ac.uk>
2017-10-19 00:24:53 +0100

    more detailed attempt on ex9 after making a flow chart

--- Week3/ex9.py
+++ Week3/ex9.py
@@ -20,22 +20,36 @@ savings_rate        = float(0)                                              #def
 
 
 ## define computation loops ##
+savings_rate = 1                        #maximum percentage to save = 100%
 epsilon = 100                           #acceptable margin of error: £100
 num_guesses = 0                         #number of iterations
 low = 0                                 #low threashold for bisection
-high = deposit                          #set high threashold as the value of the cube to be found
+high = savings_rate                     #set high threashold as the value of the cube to be found
 guess = (high + low)/2.0                #find the half point to start the check and use it to make a guess
 
-while abs(guess**3 - cube) >= epsilon:  #start a loop which checks if the guess is correct within epsilon margin
-    if guess**3 < cube:                 #if guess guess cubed is less than guess, tharefore the guess is too low
-        low = guess                     #so set the new low threashold as the guess
+
+##main loop##
+
+
+while abs(current_savings - deposit) <= epsilon:  #start a loop which checks if the guess is correct within epsilon margin
+    if current_savings < deposit:                 #if guess guess cubed is less than guess, tharefore the guess is too low
+        low = savings_rate                     #so set the new low threashold as the guess
     else:                               #if not then that means the guess is too high
-        high = guess                    #so set the new high threashold as the guess
-    guess = (high + low) / 2.0          # make a new Bisection
+        high = savings_rate                    #so set the new high threashold as the guess
+    savings_rate = (high + low) / 2.0          # make a new Bisection
     num_guesses += 1                    #increase the iteration counter
 
+    while months_passed < total_months:
+        ammount_saved += ammount_saved * interest_rate #add interest
+        ammount_saved += savings_rate*monthly_salary   #add savings
+
+        if months_passed % 6 == 0 :                                 #check for semi anual raise
+            annual_salary  += annual_salary*semi_annual_raise
+            monthly_salary  = annual_salary/12
+        months_passed += 1
+
+
 print('num_guesses =', num_guesses)                 #output number of iterations
-print(guess, 'is close to the cube root of', cube)   #output answer
+print(savings_rate, 'is the rate at which you need to save')   #output answer
 
 while months_passed < total_months:
-    
