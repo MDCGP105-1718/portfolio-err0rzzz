68092a46d43c34b2d70f1a0ffcf2754210a2a0ef
err0rzzz <s190988@uos.ac.uk>
2017-10-19 00:00:41 +0100

    got the test code working and started on ex9

---
+++ Week3/Bisection_search_example.py
@@ -0,0 +1,19 @@
+## this is the Bisection search example from the lecture slides ##
+
+cube = 27                               #number to find root of
+epsilon = 0.01                          #acceptable margin of error
+num_guesses = 0                         #number of iterations
+low = 0                                 #low threashold for bisection
+high = cube                             #set high threashold as the value of the cube to be found
+guess = (high + low)/2.0                #find the half point to start the check and use it to make a guess
+
+while abs(guess**3 - cube) >= epsilon:  #start a loop which checks if the guess is correct within epsilon margin
+    if guess**3 < cube:                 #if guess guess cubed is less than guess, tharefore the guess is too low
+        low = guess                     #so set the new low threashold as the guess
+    else:                               #if not then that means the guess is too high
+        high = guess                    #so set the new high threashold as the guess
+    guess = (high + low) / 2.0          # make a new Bisection
+    num_guesses += 1                    #increase the iteration counter
+
+print('num_guesses =', num_guesses)                 #output number of iterations
+print(guess, 'is close to the cube root of', cube)   #output answer
--- Week3/ex9.py
+++ Week3/ex9.py
@@ -5,28 +5,37 @@ print ("lets calculate how what percenatge of income is needed to save for a hou
 annual_salary       = float(input ("Enter your annual salary: "))
 
 #define variables#
-total_cost          = float(1000000))                                       #set cost of house
+total_cost          = 1000000                                               #set cost of house
 semi_annual_raise   = float(0.07)                                           #set semi annual raise
 interest_rate       = 0.04                                                  #rate of return
-months_passed       = 36                                                    #define months passed
+total_months        = 36                                                    #define months passed
+months_passed       = 0
 
-portion_deposit     = (total_cost / 100) * 25                               #set deposit to 25% of house cost
+deposit             = (total_cost / 100) * 25                               #set deposit to 25% of house cost
 monthly_salary      = annual_salary/12                                      #set monthly salary
-monthly_contrib     = monthly_salary * portion_saved                        #set ammount to increase savings by each month
+#monthly_contrib     = monthly_salary * savings_rate                         #set ammount to increase savings by each month
 current_savings     = float(0)                                              #define savings as a float
-portion_saved       = float(0)                                              #defne percentage to save as a float
+savings_rate        = float(0)                                              #defne percentage to save as a float
 
 
 
-#computation loop#
-while current_savings < portion_deposit:
-    current_savings  = current_savings + current_savings*interest_rate/12    #add interest from previous months
-    current_savings  = current_savings + monthly_contrib                     #add monthly contribution from salary
-    months_passed    = months_passed   + 1                                   #increment month counter
+## define computation loops ##
+epsilon = 100                           #acceptable margin of error: £100
+num_guesses = 0                         #number of iterations
+low = 0                                 #low threashold for bisection
+high = deposit                          #set high threashold as the value of the cube to be found
+guess = (high + low)/2.0                #find the half point to start the check and use it to make a guess
 
-    if months_passed % 6 == 0 :
-        annual_salary  += annual_salary*semi_annual_raise
-        monthly_salary  = annual_salary/12
-        monthly_contrib = monthly_salary * portion_saved
+while abs(guess**3 - cube) >= epsilon:  #start a loop which checks if the guess is correct within epsilon margin
+    if guess**3 < cube:                 #if guess guess cubed is less than guess, tharefore the guess is too low
+        low = guess                     #so set the new low threashold as the guess
+    else:                               #if not then that means the guess is too high
+        high = guess                    #so set the new high threashold as the guess
+    guess = (high + low) / 2.0          # make a new Bisection
+    num_guesses += 1                    #increase the iteration counter
 
-print (f"Number of months: {months_passed}")                                #output number of months
+print('num_guesses =', num_guesses)                 #output number of iterations
+print(guess, 'is close to the cube root of', cube)   #output answer
+
+while months_passed < total_months:
+    
