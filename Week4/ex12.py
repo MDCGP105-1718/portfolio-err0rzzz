ad89de2b7986943f7ae814d0c9acd9c050691c14
err0rzzz <s190988@uos.ac.uk>
2017-10-19 12:25:37 +0100

    finished for todays session, will complete ex12 at home

---
+++ Week4/ex12.py
@@ -0,0 +1,11 @@
+## hotel costs ##
+
+cost_per_night = int(70)
+total_hotel_cost = int(0)
+nights = int(input ('enter number of nights'))
+
+def hotel_cost (nights):
+    cost_per_night * nights = total_hotel_cost
+    return total_hotel_cost
+
+def planet_ticket_cost(city, )
