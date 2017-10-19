d9abb699d2d52fd98de1925676f45bcda0f56416
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-19 10:23:32 +0100

    major rework, removing duplicated and needless functions

--- Week4/ex10.py
+++ Week4/ex10.py
@@ -1,33 +1,6 @@
 ##function definition testing ##
 x = 1
 
-def f_fizz (x):
-
-    """
-    Checks if number is divisible by 3
-    outputs Fizz if it is
-    """
-    if x % 3 == 0:
-        print ("Fizz")
-    else:
-        print (x)
-
-    return (x)
-
-def f_buzz(x):
-
-    """
-    Checks if number is divisible by 5
-    outputs Buzz if it is
-    """
-
-    if x % 5 == 0:
-        print ("Buzz")
-    else:
-        print (x)
-
-    return (x)
-
 def f_fizzbuzz(x):
     """
     checks if number is divisible by both 3 and 5
@@ -36,10 +9,12 @@ def f_fizzbuzz(x):
     if x %3 == 0:
         if x %5 == 0:
             print ("fizzbuzz")
+        else print ("Fizz")
+    elif x %5 == 0:
+        print ("Buzz")
+    else print (x)
     return x
 
 while (x<100):
-    f_fizz(x)
-    f_buzz(x)
     f_fizzbuzz(x)
     x += 1
