3b09ed10ca8cc6655d4e43f0fd40f4879ae0e86f
err0rzzz <s190988@uos.ac.uk>
2017-10-19 11:09:05 +0100

    corrected ex11 to meet brief and is now correct

--- Week4/ex11.py
+++ Week4/ex11.py
@@ -1,15 +1,20 @@
 ##Random implimentation##
 from random import randint
-
 x = randint (0,100)
 
 guess            = int(input ('guess a number between 0-100:'))
+y = 1
 
-if guess == x:
-    print ("you got it!")
+def check_guess(x):
+    if guess > x:
+        print ("too high")
+    elif guess < x:
+        print ("too low")
+    return (x)
 
-elif guess > x:
-    print ("too high")
-
-else:
-    print ("too low")
+while guess - x !=0:
+    check_guess(x)
+    guess            = int(input ('enter a new guess: '))
+    y += 1
+    if guess == x:
+        print (f"you got it! in {y} guesses")
