41f43db27af823b336bb12618527c708ebc69627
err0rzzz <s190988@uos.ac.uk>
2017-11-02 11:10:36 +0000

    fibonacci fully working.

--- Week6/fibonacci.py
+++ Week6/fibonacci.py
@@ -3,9 +3,11 @@
 def fib(n):
     if (n == 0):
         return 0
-    elif (n == 0):
+    elif (n == 1):
         return 1
     else:
         return fib(n-1) + fib(n-2)
 
-fib(5)
+print (fib(9))
+#for n in range (20):
+#    print (fib(n))
