89897914fc31c5283add131fcc152b0b0af91c3c
err0rzzz <s190988@uos.ac.uk>
2017-11-23 13:45:57 +0000

    sorts cards random but bug possible if rand is duplicated,building function to fix, stats ln 28

--- Week09/task_01.py
+++ Week09/task_01.py
@@ -25,16 +25,18 @@ def build_deck():
     for n in range(4):
         for s in range(13):
             # deck.append((Card(s,n), random.randint(0,51)))                #deck as a list
-            deck.append((random.randint(0,52), (Card(s,n))))
+            randint = randomint()
+            deck.append((random.randint(0,10000), (Card(s,n))))
             # deck[Card(s,n)] = random.randint(0,51)      #deck as a dictionary with card as key and place in deck as value
 
 build_deck()
+deck.sort()
 
 # print (deck[5].number, deck[5].suit, deck[5])
 print (len(deck))
 
-for (n,p) in deck:
-    print (str(deck[n]) + str(deck[p]))
+for (p,c) in deck:
+    print (str(p) + " is position for "+ str(c))
 
 
 # for card,v in deck.items():
